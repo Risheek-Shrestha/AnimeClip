7e6457781adc71ed70244a242acc2d636b60fc70
Risheek Shrestha <shrestharisheek@gmail.com>
2026-05-01 10:58:45 +0530

    removed admin url

--- ananimeclip/urls.py
+++ ananimeclip/urls.py
@@ -4,13 +4,11 @@ from django.contrib.auth import views as auth_views
 from ananimeclip import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),  # was missing
     path('', views.index, name='index'),
     path('login/', views.login_view, name='login'),
     path('signup/', views.signup, name='signup'),
     path('streaming/<int:episode_id>/', views.streaming, name='streaming'),
     path('streaming_movie/<int:movie_id>/', views.streaming_movie, name='streaming_movie'),
-    # fixed: added movie_id param + correct name
 
     # comment actions
     path('episode/<int:episode_id>/comment/', views.add_comment, name='add_comment'),
